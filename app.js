070fbcd0c687fd686549d7f236aac56f13bdfcd5
toddehalexander <alexandertoddeh@gmail.com>
2023-02-14 16:36:00 -0800

    added theme compatibility

--- app.js
+++ app.js
@@ -1,31 +1,39 @@
 let main = document.getElementById("mainContent");
 let text = document.getElementById("text-to-type");
+let body = document.body;
 
 function CoralTheme() 
 {
-    var element = document.body;
-    element.classList.toggle("coralStyle"); 
-    element.classList.remove("blueStyle");
-    text.style.color = "red";
+    main.style.background = "#fbb666";
+    text.style.color = "#55231d";
+    body.style.background ="#f98d44";
 
-}
-
-function OceanTheme() {
-    main.style.backgroundColor = "blue";
 }
 
 function BlueTheme() 
 {
+    main.style.background = "#7fcdff";
+    body.style.background = "#064273";
+    text.style.color = "white";
 
-    var element = document.body;
-    element.classList.toggle("blueStyle");
-    element.classList.remove("coralStyle");
-}
 
-function DefultTheme() 
+
+}function DefultTheme() 
 {
-    var element = document.body;
-    element.classList.remove("coralStyle");
-    element.classList.remove("blueStyle");
+    main.style.background = "#404040";
+    body.style.background = "#333333";
+    text.style.color = "white";
+
 
 }
+
+// .coralStyle {
+//     background-color: coral;
+//     color: white;
+//   }
+  
+//   .blueStyle {
+//     background-color: blue;
+//     color: white;
+//   }
+  
