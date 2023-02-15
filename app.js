0c0ab2592e88d4fe7414188898d902b324c08afc
jpope6 <imthepope@csu.fullerton.edu>
2023-02-14 16:03:13 -0800

    outline for changing color of a specific id

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
+let main = document.getElementById("mainContent");
+let text = document.getElementById("text-to-type");
+
 function CoralTheme() 
 {
     var element = document.body;
     element.classList.toggle("coralStyle"); 
     element.classList.remove("blueStyle");
+    text.style.color = "red";
 
+}
+
+function OceanTheme() {
+    main.style.backgroundColor = "blue";
 }
 
 function BlueTheme() 
