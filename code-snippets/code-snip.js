c9008cbfbed421969de1b950410680089e7cf654
jpope6 <imthepope@csu.fullerton.edu>
2023-02-25 13:14:15 -0800

    Added more code snippets

--- code-snippets/code-snip.js
+++ code-snippets/code-snip.js
@@ -1,15 +1,15 @@
 import { binary_search } from "./binary-search.js";
+import { partition, quicksort } from "./quicksort.js";
 
 let code = document.getElementById("text-to-type");
 let python = document.getElementById("python");
 let java = document.getElementById("java");
 let cpp = document.getElementById("c++");
 let js = document.getElementById("js");
-let c = document.getElementById("c");
 
 
 
-const code_list = [binary_search]
+const code_list = [binary_search, partition, quicksort]
 let cur_language = "Python"
 let random_index = Math.floor(Math.random() * code_list.length);
 code.innerHTML = code_list[random_index][cur_language];
@@ -23,5 +23,4 @@ python.addEventListener("click", () => changeLanguage("Python"));
 java.addEventListener("click", () => changeLanguage("Java"));
 cpp.addEventListener("click", () => changeLanguage("C++"));
 js.addEventListener("click", () => changeLanguage("JavaScript"));
-c.addEventListener("click", () => changeLanguage("C"));
 
