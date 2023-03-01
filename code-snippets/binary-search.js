df63bd70001fbd0dbd4f103b5929a7526ce6415a
jpope6 <imthepope@csu.fullerton.edu>
2023-02-28 16:48:59 -0800

    new logic for textarea

--- code-snippets/binary-search.js
+++ code-snippets/binary-search.js
@@ -1,17 +1,17 @@
 
 export const binary_search = {
-    "Python": "def binary_search(arr, target):\n"
-        + "    left = 0\n"
-        + "    right = len(arr) - 1\n"
-        + "    while left <= right:\n"
-        + "        mid = (left + right) // 2\n"
-        + "        if arr[mid] == target:\n"
-        + "            return mid\n"
-        + "        elif arr[mid] < target:\n"
-        + "            left = mid + 1\n"
-        + "        else:\n"
-        + "            right = mid - 1\n"
-        + "    return -1",
+    "Python": ["def binary_search(arr, target):\n",
+        "    left = 0\n",
+        "    right = len(arr) - 1\n",
+        "    while left <= right:\n",
+        "        mid = (left + right) // 2\n",
+        "        if arr[mid] == target:\n",
+        "            return mid\n",
+        "        elif arr[mid] < target:\n",
+        "            left = mid + 1\n",
+        "        else:\n",
+        "            right = mid - 1\n",
+        "    return -1"],
     "Java": "public int binarySearch(int[] arr, int target) {\n"
         + "    int left = 0;\n"
         + "    int right = arr.length - 1;\n"
@@ -58,4 +58,3 @@ export const binary_search = {
         + "    return -1;\n"
         + "}",
 }
-
--- code-snippets/code-snip.js
+++ code-snippets/code-snip.js
@@ -1,5 +1,5 @@
 import { binary_search } from "./binary-search.js";
-import { partition, quicksort } from "./quicksort.js";
+// import { partition, quicksort } from "./quicksort.js";
 
 let code = document.getElementById("text-to-type");
 let python = document.getElementById("python");
@@ -9,10 +9,26 @@ let js = document.getElementById("js");
 
 
 
-const code_list = [binary_search, partition, quicksort]
+const code_list = [binary_search]
 let cur_language = "Python"
 let random_index = Math.floor(Math.random() * code_list.length);
-code.innerHTML = code_list[random_index][cur_language];
+let current_block = code_list[random_index][cur_language]
+
+let code_container = document.createElement("div");
+code_container.id = "code_container";
+
+for (let i = 0; i < current_block.length; i++) {
+    let current_line = document.createElement("div");
+    current_line.className = "line";
+    for (let j = 0; j < current_block[i].length; j++) {
+        let current = document.createElement("div");
+        current.innerHTML = current_block[i][j];
+        current_line.appendChild(current);
+    }
+    code_container.appendChild(current_line);
+}
+
+code.appendChild(code_container);
 
 function changeLanguage(language) {
     cur_language = language;
@@ -23,4 +39,5 @@ python.addEventListener("click", () => changeLanguage("Python"));
 java.addEventListener("click", () => changeLanguage("Java"));
 cpp.addEventListener("click", () => changeLanguage("C++"));
 js.addEventListener("click", () => changeLanguage("JavaScript"));
+
 
