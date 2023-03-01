89e3e60aeac4214a412ec78063ab042317ed409d
jpope6 <imthepope@csu.fullerton.edu>
2023-02-28 19:16:09 -0800

    Logic is working for different languages

--- code-snippets/code-snip.js
+++ code-snippets/code-snip.js
@@ -13,26 +13,17 @@ const code_list = [binary_search]
 let cur_language = "Python"
 let random_index = Math.floor(Math.random() * code_list.length);
 let current_block = code_list[random_index][cur_language]
+export let divArray = []
 
-let code_container = document.createElement("div");
-code_container.id = "code_container";
-
-for (let i = 0; i < current_block.length; i++) {
-    let current_line = document.createElement("div");
-    current_line.className = "line";
-    for (let j = 0; j < current_block[i].length; j++) {
-        let current = document.createElement("div");
-        current.innerHTML = current_block[i][j];
-        current_line.appendChild(current);
-    }
-    code_container.appendChild(current_line);
-}
-
-code.appendChild(code_container);
+let code_container = splitSnippetToDivs(divArray);
 
 function changeLanguage(language) {
     cur_language = language;
-    code.innerHTML = code_list[random_index][cur_language];
+    current_block = code_list[random_index][cur_language]
+
+    code_container.remove();
+
+    code_container = splitSnippetToDivs();
 }
 
 python.addEventListener("click", () => changeLanguage("Python"));
@@ -40,4 +31,30 @@ java.addEventListener("click", () => changeLanguage("Java"));
 cpp.addEventListener("click", () => changeLanguage("C++"));
 js.addEventListener("click", () => changeLanguage("JavaScript"));
 
+function splitSnippetToDivs() {
+    // Create new code container and divArray for new snippet
+    let code_container = document.createElement("div");
+    code_container.className = "code_container";
+    divArray = []
 
+
+    //loop through each line of the code snippet
+    for (let i = 0; i < current_block.length; i++) {
+        //Create a new line div
+        let current_line = document.createElement("div");
+        current_line.style.display = "flex";
+        //loop through each character in the line
+        for (let j = 0; j < current_block[i].length; j++) {
+            //Create a new current chracter div and add it to array
+            let current = document.createElement("div");
+            current.innerHTML = current_block[i][j];
+            current_line.appendChild(current);
+            divArray.push(current);
+        }
+        code_container.appendChild(current_line);
+    }
+
+    code.appendChild(code_container);
+
+    return code_container;
+}
