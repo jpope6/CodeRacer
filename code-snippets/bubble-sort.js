bf6235fa99f926a657d6e177e9dc71336eb0a8d0
jpope6 <imthepope@csu.fullerton.edu>
2023-05-02 16:14:03 -0700

    Typo in a code snippet

--- code-snippets/bubble-sort.js
+++ code-snippets/bubble-sort.js
@@ -4,9 +4,9 @@ export const bubbleSort = {
         "def bubble_sort(arr):\n",
         "    n = len(arr)\n",
         "    for i in range(n):\n",
-        "        for j in range(0, n-i-1):\n",
-        "            if arr[j] > arr[j+1]:\n",
-        "                arr[j], arr[j+1] = arr[j+1], arr[j]"
+        "        for j in range(0, n - i - 1):\n",
+        "            if arr[j] > arr[j + 1]:\n",
+        "                arr[j], arr[j + 1] = arr[j + 1], arr[j]"
     ],
     "Java": [
         "void bubbleSort(int arr[]) {\n",
